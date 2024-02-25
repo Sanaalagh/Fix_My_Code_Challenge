cbdcbd50d65cf4d638067b3f194021f5ab8bc27f
Sanaalagh <Sanaalaghzou@gmail.com>
2024-02-25 13:53:33 -0800

    fix the code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,30 +1,26 @@
 #!/usr/bin/node
 /*
- *     Print a square with the character #
- *
- *         The size of the square must be the first argument
- *             of the program.
- *             */
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
 
-if (process.argv.length !== 3) {
-	    process.stderr.write("Missing argument\n");
-	    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-	    process.stderr.write("Example: ./1-print_square.js 8\n");
-	    process.exit(1);
+    change in size = parseInt(process.argv[2], 16)
+*/
+
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
 }
 
-const size = parseInt(process.argv[2], 10);
+size = parseInt(process.argv[2], 10)
 
-if (isNaN(size) || size <= 0) {
-	    process.stderr.write("Invalid size\n");
-	    process.exit(1);
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
-
-for (let i = 0; i < size; i++) {
-	    let row = '';
-	    for (let j = 0; j < size; j++) {
-		            row += '#';
-		        }
-	    console.log(row);
-}
-
